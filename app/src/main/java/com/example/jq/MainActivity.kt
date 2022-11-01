3c5ce2eb9f0e73eac8c793b370c1c6a982cd1bd2
aeedaa <200103275@stu.sdu.edu.kz>
2022-11-01 23:06:46 +0600

    created to move from one activity to second one

--- app/src/main/java/com/example/jq/MainActivity.kt
+++ app/src/main/java/com/example/jq/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.jq
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+        //initialize
+        val mButton = findViewById<View>(R.id.button1) as Button
+        //handle onClick
+        mButton.setOnClickListener {
+            //intent to start NewActivity
+            startActivity(Intent(this@MainActivity, LoginPage::class.java))
+        }
     }
 }
